cbc47c4367fe510e96e682fdc3434d1874b93de2
alexook <gilber00774@gmail.com>
2023-05-07 15:35:10 +0800

    New commit

--- src/yieldmap/src/yield_node.cpp
+++ src/yieldmap/src/yield_node.cpp
@@ -1,20 +1,17 @@
 #include "yield_map.hpp"
 
-
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "yieldmap");
     ros::NodeHandle nh;
 
-    YieldMap yieldmap(nh);
+    YieldMap yield(nh);
+
     ros::Rate rate(10);
     while (ros::ok())
     {
-        ROS_INFO("yieldmap node running...");
         ros::spinOnce();
         rate.sleep();
     }
-    
     return 0;
 }
-
